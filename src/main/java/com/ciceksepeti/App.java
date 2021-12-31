0a8e54dae9449d81189de946a4d462b96619b97d
canoklay <fatihcan.oklay@vigo.ist>
2021-12-31 09:43:24 +0300

    maven and gitignore

---
+++ src/main/java/com/ciceksepeti/App.java
@@ -0,0 +1,13 @@
+package com.ciceksepeti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
