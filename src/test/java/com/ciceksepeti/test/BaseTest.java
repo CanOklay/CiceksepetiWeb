a3e122cccb34ed9f3ba6d849af27a848fc6e9bfb
canoklay <fatihcan.oklay@vigo.ist>
2022-01-09 12:54:19 +0300

    configuration properties and dynamic environment

--- src/test/java/com/ciceksepeti/test/BaseTest.java
+++ src/test/java/com/ciceksepeti/test/BaseTest.java
@@ -1,14 +1,18 @@
 package com.ciceksepeti.test;
 
+import com.ciceksepeti.config.ConfigFileReader;
+import com.ciceksepeti.utility.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 /**
@@ -16,29 +20,28 @@ import java.time.Duration;
  * Date: 30.12.2021
  */
 
-public class BaseTest {
+public abstract class BaseTest extends DriverFactory {
 
-    //protected static RemoteWebDriver driver; //for docker
-    //protected static String remoteUrl = "http://localhost:4444/wd/hub"; //for docker
-    protected static WebDriver driver;
-    protected static String baseUrl = "https://www.lolaflora.com/login";
-    protected static WebDriverWait webDriverWait;
+    ConfigFileReader configFileReader = new ConfigFileReader();
 
     @BeforeMethod
     public void setup() throws MalformedURLException {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("disable-popup-blocking");
-        options.addArguments("disable-translate");
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        //driver = new RemoteWebDriver(new URL(remoteUrl), options); //for docker
-        driver.navigate().to(baseUrl);
+        if (configFileReader.getEnv().equals("local")) {
+            if (configFileReader.getBrowser().equals("opera")) {
+                operaDriver();
+            } else if (configFileReader.getBrowser().equalsIgnoreCase("firefox")) {
+                firefoxDriver();
+            } else {
+                chromeDriver();
+            }
+        } else {
+            remoteWebDriver();
+        }
+        driver.navigate().to(configFileReader.getBaseUrl());
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
-        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(configFileReader.getImplicityWait()));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(configFileReader.getPageLoadTimeout()));
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(configFileReader.getWebDriverWait()));
     }
 
     @AfterMethod
